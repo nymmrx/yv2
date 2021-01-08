de46e289a0626c9880170343e664bdfc11e2dd3c
nymmrx <nymmrx@protonmail.com>
2021-01-08 09:41:54 +0100

    feat(plsmoney): plsmoney works

--- testenv/src/consts.js
+++ testenv/src/consts.js
@@ -1,3 +1,8 @@
-export const DaiAddress = "0x6B175474E89094C44Da98b954EedeAC495271d0F";
-export const WethAddress = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2";
+import UniV2RouterABI from "./abi/univ2/router.abi.js";
+
+export const uniswap = {
+  address: "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D",
+  abi: UniV2RouterABI,
+};
+
 export const ChainID = 1337;
--- testenv/src/plsmoney.js
+++ testenv/src/plsmoney.js
@@ -1,11 +1,39 @@
-import log from "signale";
-
-import { Fetcher, TokenAmount } from "@uniswap/sdk";
+import signale from "signale";
 
 import { JsonRpcProvider } from "@ethersproject/providers";
+import { Contract } from "@ethersproject/contracts";
+import { MaxUint256 } from "@ethersproject/constants";
+import erc20 from "@studydefi/money-legos/erc20";
+
+import {
+  parseEther,
+  parseUnits,
+  formatEther,
+  formatUnits,
+} from "@ethersproject/units";
 
 import { Address } from "./settings";
-import { DaiAddress, WethAddress, ChainID } from "./consts";
+import { uniswap } from "./consts";
+
+const log = new signale.Signale({
+  types: {
+    info: {
+      badge: "🔼",
+      color: "blue",
+      label: "info",
+    },
+    swap: {
+      badge: "🔀",
+      color: "yellow",
+      label: "swap",
+    },
+    wrap: {
+      badge: "🎁",
+      color: "magenta",
+      label: "wrap",
+    },
+  },
+});
 
 async function main() {
   log.info("Generating tokens for", Address);
@@ -13,25 +41,58 @@ async function main() {
   const provider = new JsonRpcProvider();
   const signer = provider.getSigner(Address);
 
-  log.info("ETH balance:", (await signer.getBalance()).toString());
+  const { weth, dai } = erc20;
 
-  const WETH = await Fetcher.fetchTokenData(
-    ChainID,
-    WethAddress,
-    provider,
-    "WETH"
-  );
-  const DAI = await Fetcher.fetchTokenData(
-    ChainID,
-    DaiAddress,
-    provider,
-    "DAI"
-  );
+  const WETH = new Contract(weth.address, weth.abi, signer);
+  const DAI = new Contract(dai.address, dai.abi, signer);
 
-  const WETH_DAI = await Fetcher.fetchPairData(WETH, DAI, provider);
+  await WETH.approve(uniswap.address, MaxUint256);
 
-  const [amount] = WETH_DAI.getOutputAmount(new TokenAmount(WETH, "100"));
-  console.log(amount.toExact());
+  async function portfolio() {
+    return {
+      eth: await signer.getBalance(),
+      weth: await WETH.balanceOf(Address),
+      dai: await DAI.balanceOf(Address),
+    };
+  }
+
+  let balance = await portfolio();
+
+  log.info("ETH balance:", formatEther(balance.eth));
+
+  const Uniswap = new Contract(uniswap.address, uniswap.abi, signer);
+
+  const wethThreshold = parseEther("5");
+  if (balance.eth.gt(balance.weth)) {
+    if (balance.eth.lt(wethThreshold)) {
+      log.fatal("Your eth balance is < 5. Restart the testchain...");
+      process.exit(1);
+    }
+
+    const toWrap = balance.eth.sub(wethThreshold);
+    await WETH.deposit({ value: toWrap });
+    balance = await portfolio();
+    log.wrap("ETH -> wETH", formatEther(balance.weth));
+  }
+
+  log.info("wETH balance:", formatEther(balance.weth));
+
+  const daiThreshold = parseUnits("10000", dai.decimals);
+  if (balance.dai.lt(daiThreshold)) {
+    const wethT = parseUnits("15", weth.decimals);
+    const deadline = (await provider.getBlock("latest")).timestamp + 1200;
+    const res = await Uniswap.swapExactTokensForTokens(
+      wethT,
+      0,
+      [weth.address, dai.address],
+      Address,
+      deadline
+    );
+    balance = await portfolio();
+    log.swap("wETH -> DAI", formatUnits(balance.dai.toString(), dai.decimals));
+  }
+
+  log.info("DAI balance:", formatUnits(balance.dai.toString(), dai.decimals));
 }
 
 main();
